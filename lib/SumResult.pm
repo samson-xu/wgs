503a78f43eab381311830e99ac605c6518a62324
samson-xu <xy_xu@foxmail.com>
2020-08-24 16:01:19 +0800

    update sumresult pm

--- lib/SumResult.pm
+++ lib/SumResult.pm
@@ -16,10 +16,10 @@ sub copy {
 	my $sampleId = shift;
 	system("mkdir -p $outDir") == 0 || die $!;
 	my $copy_shell = "#script for copy result file\n";
-	my $copy_shell .= "cp $inDir/03.snp_indel/$sampleId.*.vcf.gz $outDir" if ($step =~ m/v/);
-	my $copy_shell .= "cp $inDir/04.sv/manta/*.tsv $outDir" if ($step =~ m/s/);
-	my $copy_shell .= "cp $inDir/04.sv/fusion/*.xls $outDir" if ($step =~ m/u/);
-	my $copy_shell .= "cp $inDir/05.mtdna/*.xls $outDir" if ($step =~ m/m/);
-	my $copy_shell .= "cp $inDir/../*cnv/cnv/$sampleId*tsv $outDir" if ($step =~ m/n/);
+	$copy_shell .= "cp $inDir/03.snp_indel/$sampleId.*.vcf.gz $outDir\n" if ($step =~ m/v/);
+	$copy_shell .= "cp $inDir/04.sv/manta/*.tsv $outDir\n" if ($step =~ m/s/);
+	$copy_shell .= "cp $inDir/04.sv/fusion/*.xls $outDir\n" if ($step =~ m/u/);
+	$copy_shell .= "cp $inDir/05.mtdna/*.xls $outDir\n" if ($step =~ m/m/);
+	$copy_shell .= "cp $inDir/../*cnv/cnv/$sampleId*tsv $outDir\n" if ($step =~ m/n/);
 	write_shell($copy_shell, "$inDir/$sampleId.summary.sh");
 }
